4f95f348ecbe7235452d3ed16841d96abd758d10
NguyenDuongQuang <ndq1234pn@gmail.com>
2023-12-21 00:04:49 +0700

    fix_angular_project
    fix_angular_project

--- font-end-admin/src/app/component/model/UsersDTO.ts
+++
@@ -1,8 +0,0 @@
-export class UsersDTO{
-  id: number;
-  username: string;
-  email: string;
-  id_customer: number;
-  id_staff: number;
-  role: string;
-}
--- font-end-view/src/app/component/model/UsersDTO.ts
+++
@@ -1,9 +0,0 @@
-export class UsersDTO {
-  id: number;
-  fullname: string;
-  username: string;
-  email: string;
-  id_customer: number;
-  id_staff: number;
-  role: string;
-}
